7b61f76ac04453b130d637d84b3111d0a31f493c
Piotr Maziarz <piomaziarz@gmail.com>
2018-06-02 17:44:08 +0200

    project files

---
+++ ImpulseDamping/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace ImpulseDamping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var myModel = new PlotModel { Title = "Example 1" };
+            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            this.plot1.Model = myModel;
+        }
+    }
+}
